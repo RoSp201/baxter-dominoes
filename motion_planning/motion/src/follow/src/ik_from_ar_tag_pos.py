04a178f1c220c3deba3408dab611d8e1d0270c77
Robert Spark <rspark@berkeley.edu>
2016-11-23 00:36:41 -0800

    have basic example movement working, need to finish constraints

--- motion_planning/motion/src/follow/src/ik_from_ar_tag_pos.py
+++ motion_planning/motion/src/follow/src/ik_from_ar_tag_pos.py
@@ -3,14 +3,11 @@ import sys
 import tf
 import rospy
 from tf2_msgs.msg import TFMessage
-from geometry_msgs.msg import Transform, Vector3, Twist
-import exp_quat_func as eqf
 import ar_tag_pos as arp
 from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse
 from geometry_msgs.msg import PoseStamped
 from moveit_commander import MoveGroupCommander
 import numpy as np
-from numpy import linalg
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
 global subscriber
@@ -19,13 +16,6 @@ global subscriber
 #try and move his left gripper to just above that location (the position of the AR tag in base frame)
 
 def follow(msg):
-
-    li = tf.TransformListener()
-    #transform frame coords, so with respect to base frame
-    (trans, rot)  = li.lookupTransform("/cameras/left_hand_camera/left_hand_camera", "/cameras/left_hand_camera/base", rospy.Time(0))
-    rbt = arp.return_rbt(trans=trans, rot=rot)
-    
-    global subscriber
     
     #Wait for the IK service to become available
     rospy.wait_for_service('compute_ik') 
@@ -33,35 +23,19 @@ def follow(msg):
     compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
     
     marker = msg.markers[0]
-    x = marker.pose.pose.position.x
-    y = marker.pose.pose.position.y
-    z = marker.pose.pose.position.z
-    print "Cam coordinates: ", x, y, z
+    x1 = marker.pose.pose.position.x
+    y1 = marker.pose.pose.position.y
+    z1 = marker.pose.pose.position.z
+
     #collected coordinates are observered with respect to camera frame.
-    coords_cam_frame = np.array([x,y,z,1])
-
-    base_coords = rbt.dot(coords_cam_frame)
-    print "Base coordinates: ",  base_coords
-
-    #this will help pause 
-    raw_input("Hit enter") 
+    coords_cam_frame = np.array([x1,y1,z1,1])
 
     #Construct the request
     request = GetPositionIKRequest()
     request.ik_request.group_name = "left_arm"
     request.ik_request.ik_link_name = "left_gripper"
-    request.ik_request.attempts = 10
-    request.ik_request.pose_stamped.header.frame_id = "base"
-
-    #Set the desired orientation for the end effector HERE
-    request.ik_request.pose_stamped.pose.position.x = 0.5#x + 0.3
-    request.ik_request.pose_stamped.pose.position.y = 0.0
-    request.ik_request.pose_stamped.pose.position.z = 0.5 #z + 0.5
-    request.ik_request.pose_stamped.pose.orientation.x = 0.0
-    request.ik_request.pose_stamped.pose.orientation.y = -1.0
-    request.ik_request.pose_stamped.pose.orientation.z = 0.0
-    request.ik_request.pose_stamped.pose.orientation.w = 0.0    
-
+    request.ik_request.attempts = 20
+    request.ik_request.pose_stamped.header.frame_id = "base" 
 
     #orien_const = OrientationConstraint()
     #orien_const.link_name = "left_gripper";
@@ -74,41 +48,71 @@ def follow(msg):
     #consts = Constraints()
     #consts.orientation_constraints = [orien_const]
 
+    flag = False
+    tf_listener = tf.TransformListener()
+
     while not rospy.is_shutdown(): 
+
+        print "Cam coordinates: ", x1, y1, z1
         try:
-            response = compute_ik(request)
+            #transform frame coords, so with respect to base frame
+            tf_listener.waitForTransform("base", "left_hand_camera", rospy.Time(0), rospy.Duration(4.0))
+            (trans, rot) = tf_listener.lookupTransform("base", "left_hand_camera", rospy.Time(0))
+
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+            
+        rbt = arp.return_rbt(trans=trans, rot=rot)
+        base_coords = rbt.dot(coords_cam_frame)
+        base_coords = base_coords.reshape(4,1)
+
+        x2 = base_coords.item(0)
+        y2 = base_coords.item(1)
+        z2 = base_coords.item(2)
+        print "Base coordinates: ", x2, y2, z2
+
+        raw_input("Enter to execute move: ")
+
+        #Set the desired orientation for the end effector HERE
+        request.ik_request.pose_stamped.pose.position.x = x2
+        request.ik_request.pose_stamped.pose.position.y = y2
+        request.ik_request.pose_stamped.pose.position.z = z2 + 0.05
+        request.ik_request.pose_stamped.pose.orientation.x = 0.0
+        request.ik_request.pose_stamped.pose.orientation.y = -1.0
+        request.ik_request.pose_stamped.pose.orientation.z = 0.0
+        request.ik_request.pose_stamped.pose.orientation.w = 0.0  
+
+        response = compute_ik(request)
+        group = MoveGroupCommander("left_arm")
+
+        # Setting position and orientation target
+        group.set_pose_target(request.ik_request.pose_stamped)
+
+        # Setting just the position without specifying the orientation
+        #group.set_position_target([0.5, 0.0, 0.0])
+
+        # Plan IK and execute
+        if flag == False:
             print(response)
-            group = MoveGroupCommander("left_arm")
+            group.go()
+            print "tried to execute move."
+            flag = True
+        else:
+            print "\n\nAlready moved. Please Restart.\n\n"
 
-            # Setting position and orientation target
-            group.set_pose_target(request.ik_request.pose_stamped)
-
-            # Setting just the position without specifying the orientation
-            #group.set_position_target([0.5, 0.0, 0.0])
-
-            # Plan IK and execute
-            #group.go()
-            if response.error_code.val != -31:
-                flag = True
-                print("DONE!")
-
-        except rospy.ServiceException, e:
-            print "Service call failed:%s"%e
-    
-        rospy.sleep(1)
+        if response.error_code.val == 1:
+            flag = True
     subscriber.unregister()
-
-
+    
 
 def listener(ar_tags):
 
-    rospy.init_node("ik_from_ar_pos")
+    rospy.init_node("ik_from_ar_pos", anonymous=True)
     subscriber = rospy.Subscriber("ar_pose_marker", AlvarMarkers, follow)
-    #print("listener")
+    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 
-#Python's syntax for a main() method
 if __name__ == '__main__':
 
     if len(sys.argv) < 2:
@@ -116,8 +120,6 @@ if __name__ == '__main__':
     else:
         ar_tags = {}
         ar_tags['ar3'] = 'ar_marker_' + sys.argv[1]
-
-        #print('\nwas able to get here' )
         listener(ar_tags)
 
 
