60016e7a2f458f494e9fe5cb48b8fdd375103778
Andrew Blatner <ablatner@gmail.com>
2016-12-05 18:57:16 -0800

    Use AlvarMarker msg.

--- analysis/ar_tag_pos_analysis_sub.py
+++ analysis/ar_tag_pos_analysis_sub.py
@@ -3,6 +3,7 @@ import rospy
 from defaultdict import defaultdict
 import matplotlib.pyplot as plt
 import numpy as np
+from ar_track_alvar_msgs.msg import AlvarMarker
 
 
 waiting_dict = defaultdict(list)
@@ -11,16 +12,16 @@ pos_dict = defaultdict(dict)
 
 def desired_position_sub(msg):
     global waiting_dict
-    tag_num = msg.tag_num
-    tag_pos = msg.tag_pos
+    tag_num = msg.id
+    tag_pos = msg.pose.pose.position
     waiting_dict[tag_num].append(tag_pos)
 
 
 def actual_position_sub(msg):
     global waiting_dict
     global pos_dict
-    tag_num = msg.tag_num
-    actual_pos = msg.tag_pos
+    tag_num = msg.id
+    tag_pos = msg.pose.pose.position
     if tag_num in waiting_dict and len(waiting_dict[tag_num]) > 0:
         des_pos = waiting_dict[tag_num].pop(0)
         pos_dict[tag_num][des_pos] = actual_pos
@@ -43,8 +44,8 @@ def analyze_data():
 def listener():
 
     rospy.init_node("ar_tag_pos_analysis", anonymous=True)
-    rospy.Subscriber(GOAL POSITION TOPIC, TODO_MSG_TYPE, follow)
-    rospy.Subscriber(ACTUAL POSITION TOPIC, TODO_MSG_TYPE, follow)
+    rospy.Subscriber(GOAL_POSITION_TOPIC, AlvarMarker, desired_position_sub)
+    rospy.Subscriber(ACTUAL_POSITION_TOPIC, AlvarMarker, actual_position_sub)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
